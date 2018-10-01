6308ec902d6bef4c05c0b899fec4f9fb61f5412c
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2018-10-01 21:40:08 +0700

    login logout belum selesai

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+include "database.php";
+
+if (empty($_REQUEST['submit'])){ //jika tombol submit tak ada yang ditekan
+	$view = "<form action='' method='POST'>
+			Username:</br>
+			<input type='text' name='username' maxlength='6'/></br>
+			Password:</br>
+			<input type='password' name='password' maxlength='6'/></br>					
+			<p><input type='submit' name='submit' value='Login'/></p>";
+}elseif($_REQUEST['submit'] == "Login"){	
+	//simpan 
+	$sql = "select * from pengguna 
+			where username = '". $_REQUEST['username']."'
+			and password = '". sha1($_REQUEST['password'])."'";	
+	
+	$query = $mysqli->query($sql);
+	$count = $query->num_rows;
+	
+	if($count != 0)	
+		header("location:pegawai.php");	
+	else
+		$view = "data take ditemukan!. <a href='login.php'>coba lagi</a>";
+}
+echo $view;
+?>
